5276ccd27602b42df1f52b9e5e53414ff5e71fcc
festrs <felipe.dipereira@gmail.com>
2016-07-07 23:54:59 -0300

    adding bridging

---
+++ PlantInfo/BridgingObject.h
@@ -0,0 +1,13 @@
+//
+//  BridgingObject.h
+//  PlantInfo
+//
+//  Created by Felipe Dias Pereira on 2016-07-07.
+//  Copyright © 2016 Felipe Dias Pereira. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BridgingObject : NSObject
+
+@end
